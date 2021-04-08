5f32e46c370ecfde7df5ad3b757498bcfc16fc33
jacobgerun1 <44880481+jacobgerun1@users.noreply.github.com>
2021-04-08 13:55:17 -0600

    first changes

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,10 +6,10 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="Current map"/>
+      <h1>Hi from the second page</h1>
+      <p>Displays the map given to by the selected route</p>
+      <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
